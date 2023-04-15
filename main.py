80c961e93c6631a4fcef0f7f7c5803435033a836
hendrykik <Hendrykik@wp.pl>
2023-04-15 22:09:29 +0200

    Real time camera detector

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+#img = cv2.imread('lena.png')
+cap = cv2.VideoCapture(1)
+cap.set(3, 1280)#szerokosc
+cap.set(4, 720)#wysokosc
+cap.set(10, 150)#jasnosc
+thres = 0.45#od ilu procent uznajemy coś za obiekt
+nms_threshold = 0.2#ile ignorowac podobnych
+
+classNames = []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+while True:
+    success, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold = thres)
+    bbox = list(bbox)
+    confs = list(np.array(confs).reshape(1, -1)[0])
+    confs = list(map(float, confs))
+
+    indeces = cv2.dnn.NMSBoxes(bbox, confs, thres, nms_threshold)
+
+    for i in indeces:
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        cv2.rectangle(img, (x,y), (x+w, y+h), color=(0, 255, 0), thickness=2)
+        cv2.putText(img, classNames[classIds[i] - 1].upper(), (box[0] + 10, box[1] + 30),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        cv2.putText(img, str(round(confs[i]*100,2)), (box[0] + 200, box[1] + 30),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("Output", img)
+    cv2.waitKey(1)
