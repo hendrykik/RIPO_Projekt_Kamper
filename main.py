3d88cd16bc40e5b1aca61ff2479cb2126341f74d
Karol Rakicki <rakichuck@gmail.com>
2023-04-18 12:57:44 +0200

    poprawione

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import cv2
 import mediapipe as mp
-import time
 
 cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-
-#fps
-#time
-curent = 0
-previous = 0
 
 while True:
     success, img = cap.read()
@@ -23,10 +17,5 @@ while True:
         for handsMulti in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handsMulti, mpHands.HAND_CONNECTIONS)
 
-    curent = time.time()
-    fps = 1/(curent-previous)
-    previous = curent
-
-    cv2.putText(img, str(fps),(10,70), cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
